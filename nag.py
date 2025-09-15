6c26c19b35d825addd757febb2d71acf7b182ad8
santhosh <santhosh037@gmail.com>
2025-09-15 15:24:13 +0530

    sample

---
+++ nag.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import*
+b=tkinter.Tk()
+b.title("aditya")
+b.geometry("100x100")
+a=Button(b,text="Submit")
+a.pack(side=LEFT)
+a=Button(b,text="cancel")
+a.pack(side=RIGHT)
+b.mainloop()
